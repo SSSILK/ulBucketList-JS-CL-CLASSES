ca4e7f3c05618850978f2e77001d77ca2060c270
Daijohn Bryant <ohnntsendto@gmail.com>
2025-08-27 18:54:38 +0000

    Lab --Completed

--- src/main/Lab.js
+++ src/main/Lab.js
@@ -45,4 +45,12 @@ class Lightbulb {
      * TODO: Create a function named 'getDescription' that returns "The bulb is on" or "The bulb is off", depending on
      * whether 'state' is true or false.
      */
+
+    getDescription(){
+    if (this.state === true){
+        return "The bulb is on"
+    } else {
+        return " The bulb is off"
+    }
+}
 }
